63a723280ec3f1b73a3e0b9548589392b065bef1
manh2911 <manh291196@gmail.com>
2020-06-12 04:32:37 +0700

    orrrrder4

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Helper\ServiceAction;
 use App\Http\Controllers\Controller;
+use App\ImageDetailProduct;
 use App\Order;
+use App\OrderItem;
+use App\Product;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
     public function index() {
         $orders = Order::all();
-
 
         return view('Admin.managerOrder.order', compact('orders'));
     }
@@ -21,6 +24,9 @@ class OrderController extends Controller
             $order->update([
                 'status' => $request->status
             ]);
+            if ($request->status == ServiceAction::ORDER_CANCEL) {
+                $this->updateQuantityProductWhenCancelOrder($order);
+            }
 
             return response()->json([
                 'status' => true,
@@ -34,5 +40,46 @@ class OrderController extends Controller
             ]);
         }
 
+    }
+
+    public function updateQuantityProductWhenCancelOrder($order){
+        $orderItems = OrderItem::where('order_id', $order->id)->get();
+
+        foreach ($orderItems as $item) {
+            $product = Product::find($item->product_id);
+            $product->update([
+                'quantity' => $product->quantity + $item->quantity
+            ]);
+        }
+    }
+
+    public function showDetail(Request $request) {
+        try {
+            $items = OrderItem::where('order_id', $request->orderId)->get()->toArray();
+            $nameProducts = [];
+            $imageProducts = [];
+            foreach ($items as $item) {
+                $product = Product::find($item['product_id']);
+                $nameProducts[] = $product->name;
+                $image = ImageDetailProduct::where('product_id', $product->id)->first();
+                $imageProducts[] = $image->image;
+            }
+
+            $order = Order::find($request->orderId)->toArray();
+            return response()->json([
+                'status' => true,
+                'message' => 'success',
+                'items' => $items,
+                'order' => $order,
+                'nameProducts' => $nameProducts,
+                'imageProducts' => $imageProducts,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => true,
+                'message' => 'fails',
+                'error' => $e->getMessage()
+            ]);
+        }
     }
 }
--- public/Admin/js/order.js
+++ public/Admin/js/order.js
@@ -4,10 +4,6 @@ $(document).ready(function(){
     showDetail();
 });
 
-function confirmChangeStatus(status) {
-    confirm("Bạn có muốn chuyển trạng thái sang [" + status + "]");
-}
-
 function changeStatusOrder(orderId, status) {
     $.ajax({
         type: 'POST',
@@ -25,8 +21,10 @@ function changeStatusOrder(orderId, status) {
 
                 if (status == 2) {
                     $('#order-' + orderId).css("background-color", "lightgreen");
+                    $('#status-' + orderId).text('Success');
                 } else if (status == 3) {
                     $('#order-' + orderId).css("background-color", "lightgray");
+                    $('#status-' + orderId).text('Cancel');
                 } else {
 
                 }
@@ -43,21 +41,87 @@ function changeStatusOrder(orderId, status) {
 
 function changeStatusToSuccess() {
     $('.status-success').click(function () {
-        let orderId = $(this).data("id");
-        changeStatusOrder(orderId, 2, this)
+        if (confirm('Bạn có muốn chuyển trạng thái sang [SUCCESS]')) {
+            let orderId = $(this).data("id");
+            changeStatusOrder(orderId, 2, this)
+        } else {
+            return;
+        }
     })
 }
 
 function changeStatusToCancel() {
     $('.status-cancel').click(function () {
-        let orderId = $(this).data("id");
-        changeStatusOrder(orderId, 3)
+        if (confirm('Bạn có muốn chuyển trạng thái sang [CANCEL]')) {
+            let orderId = $(this).data("id");
+            changeStatusOrder(orderId, 3)
+        } else {
+            return;
+        }
+
     })
 }
 
 function showDetail() {
     $('.show-detail').click(function () {
-        $('#classModal').modal('show')
+        $('#classModal').modal('show');
+        let orderId = $(this).data("id");
+        $.ajax({
+            type: 'POST',
+            url: '/admin/order/show-detail',
+            dataType: 'json',
+            data: {
+                _token: $('meta[name="csrf-token"]').attr('content'),
+                "orderId" : orderId,
+            },
+            success: function(res) {
+                $("#classTable > tbody").empty();
+
+                if (res.status == true) {
+                    let order = res.order; // append info order
+                    let total_origin_price = order.total_origin_price.toLocaleString('us', {minimumFractionDigits: 0, maximumFractionDigits: 0}) + ' đ';
+                    let total_discount = order.total_discount.toLocaleString('us', {minimumFractionDigits: 0, maximumFractionDigits: 0}) + ' đ';
+                    let total_price = order.total_price.toLocaleString('us', {minimumFractionDigits: 0, maximumFractionDigits: 0}) + ' đ';
+                    $('#total_origin_price').text(total_origin_price);
+                    $('#total_discount').text(total_discount);
+                    $('#total_price').text(total_price);
+                    $('#receiver').text(order.name);
+                    $('#phone').text(order.phone);
+                    $('#address').text(order.address);
+
+                    let items = res.items; // append info order items
+                    let nameProducts = res.nameProducts;
+                    let imageProducts = res.imageProducts;
+                    for (let i = 0; i < items.length; i++) {
+                        let origin_price = items[i].origin_price.toLocaleString('us', {minimumFractionDigits: 0, maximumFractionDigits: 0}) + ' đ';
+                        let discount_price = items[i].discount_price.toLocaleString('us', {minimumFractionDigits: 0, maximumFractionDigits: 0}) + ' đ';
+                        let last_price = items[i].last_price.toLocaleString('us', {minimumFractionDigits: 0, maximumFractionDigits: 0}) + ' đ';
+
+                        let html = " <tr>\n" +
+                            "            <td>"+ (i + 1) +"</td>\n" +
+                            "            <td><img class=\"img-order-item\" src="+ imageProducts[i] +"></td>\n" +
+                            "            <td>"+ nameProducts[i] +"</td>\n" +
+                            "            <td>"+ items[i].quantity +"</td>\n" +
+                            "            <td>"+ origin_price +"</td>\n" +
+                            "            <td>"+ discount_price +"</td>\n" +
+                            "            <td>"+ last_price +"</td>\n" +
+                            "        </tr>"
+
+                        $("#classTable tbody").append(html);
+                    }
+
+                } else {
+                    Swal.fire({
+                        icon: 'warning',
+                        title: 'Đã có lỗi xảy ra',
+                        showConfirmButton: true,
+                    })
+                }
+            }
+        });
+
     })
 
 }
+
+
