d9a3f799b2a2aa9cc293462996601517d29d5552
manh2911 <manh291196@gmail.com>
2020-07-04 19:26:03 +0700

    edit index admin

--- resources/views/Admin/layout/index.blade.php
+++ resources/views/Admin/layout/index.blade.php
@@ -3,7 +3,7 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800">index</h1>
+    <h1 class="h3 mb-4 text-gray-800">Shop Trẻ Em</h1>
 
 </div>
 @endsection
